73c2f1d84232bea41b8ab897774f8008c2cb4b89
Meena-Sakthi <meenasruthipranav@gmail.com>
2024-05-12 19:44:52 +0530

    addPatients

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
-// import ThreeTabs from './components/ThreeTabs';
-// import Tasks from './components/Tasks';
-// import UpcomingSchedule from './components/UpComingSchduele';
-import Dashboard from './components/Dashboard/Dashboard';
-import Messages from "./components/messages/Messages"
-import SettingPage from './components/settings/SettingPage';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import AddPatients from "./components/AddPatients/addPatients";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Messages from "./components/messages/Messages";
+import SettingPage from "./components/settings/SettingPage";
 
 export default function App() {
   return (
-    <ScrollView className='bg-[#F9F9F9] '>
+    <ScrollView className="bg-[#F9F9F9] ">
       {/* <Dashboard /> */}
       {/* <SettingPage/> */}
-      <Messages/>
+      {/* <Messages/>*/}
+      <AddPatients />
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
