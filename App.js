9d54a10e236281e35575d62057e4ba9efb7545e7
vasanshettiTharun <vasanshettitharun@gmail.com>
2024-05-19 16:33:57 +0530

    dashboard_routes

--- App.js
+++ App.js
@@ -1,21 +1,142 @@
+import React from "react";
+import { Image, View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
-import AddPatients from "./components/AddPatients/addPatients";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Messages from "./components/messages/Messages";
 import SettingPage from "./components/settings/SettingPage";
-import Patients from "./components/Patients/patients";
+import dashboard from "./assets/dashboard/dashboard.png";
+import CustomDrawerContent from "./components/CustomDrawerContent";
+import setting from "./assets/dashboard/setting.png";
+import message from "./assets/dashboard/mail.png";
+import schedule from "./assets/dashboard/schedule.png";
+import task from "./assets/dashboard/task.png";
+import patients from "./assets/dashboard/people.png";
+import support from "./assets/dashboard/support.png";
+import analytic from "./assets/dashboard/analytic.png";
+import logut from "./assets/dashboard/logut.png";
+import Tasks from "./components/Tasks";
+import Patients from "./components/Patients";
+import Schedule from "./components/Schedule";
+import Analytics from "./components/Analytics";
+import Support from "./components/Support";
+import SearchBar from "./components/pages/SearchBar";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+const App = () => {
   return (
-    <ScrollView className="bg-[#F9F9F9] ">
-      {/* <Dashboard /> */}
-      {/* <SettingPage/> */}
-      {/* <Messages/>*/}
-      {/* <AddPatients /> */}
-      <Patients />
-    </ScrollView>
+    <>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+          <Drawer.Screen
+            name="dashboard"
+            component={Dashboard}
+            options={{
+              drawerLabel: "Dashboard",
+              title: "Dashboard",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => (
+                <Image source={dashboard} className="h-6 w-6" />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="settings"
+            component={Schedule}
+            options={{
+              drawerLabel: "Schedule",
+              title: "Schedule",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={schedule} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+          <Drawer.Screen
+            name="tasks"
+            component={Tasks}
+            options={{
+              drawerLabel: "Tasks",
+              title: "Tasks",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={task} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+          <Drawer.Screen
+            name="Patients"
+            component={Patients}
+            options={{
+              drawerLabel: "Patients",
+              title: "Patients",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={patients} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+          <Drawer.Screen
+            name="Messages"
+            component={Messages}
+            options={{
+              drawerLabel: "Messages",
+              title: "Messages",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={message} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+          <Drawer.Screen
+            name="Analytics"
+            component={Analytics}
+            options={{
+              drawerLabel: "Analytics",
+              title: "Analytics",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={analytic} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+
+          <Drawer.Screen
+            name="Settings"
+            component={SettingPage}
+            options={{
+              drawerLabel: "Settings",
+              title: "Settings",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={setting} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+          <Drawer.Screen
+            name="Support"
+            component={Support}
+            options={{
+              drawerLabel: "Support",
+              title: "Support",
+              headerRight: () => <SearchBar />,
+              drawerIcon: () => <Image source={support} className="h-6 w-6" />, // Apply styles using style prop
+            }}
+          />
+          <>
+            <Drawer.Screen
+              name="Logut"
+              component={Support}
+              // className='absolute bottom-0'
+              options={{
+                drawerLabel: "Logut",
+                title: "Logut",
+                headerRight: () => <SearchBar />,
+                drawerIcon: () => <Image source={logut} className="h-6 w-6" />, // Apply styles using style prop
+              }}
+            />
+          </>
+        </Drawer.Navigator>
+
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({});
+
+export default App;
--- components/AddPatients/addPatients.jsx
+++ components/AddPatients/addPatients.jsx
@@ -1,8 +1,13 @@
 import { SafeAreaView } from "react-native-safe-area-context";
+// import EvilClose from "react-native-vector-icons/EvilIcons";
+// import Calendar from "react-native-vector-icons/EvilIcons";
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
+
+import { Picker } from "@react-native-picker/picker";
+// import { Select } from "nativewind";
 
 import {
   View,
@@ -11,58 +16,13 @@ import {
   Icon,
   Button,
   Pressable,
-  Modal,
-  FlatList,
   Platform,
 } from "react-native";
 import { TextInput, ScrollView, Image } from "react-native";
 
-function CustomDropdown({ options, value, onSelect }) {
-  const [visible, setVisible] = useState(false);
-
-  const handleSelect = (option) => {
-    onSelect(option);
-    setVisible(false);
-  };
-
-  return (
-    <View>
-      <TouchableOpacity
-        onPress={() => setVisible(true)}
-        className="p-2 border border-gray-300 rounded"
-      >
-        <Text>{value || "Select Gender"}</Text>
-      </TouchableOpacity>
-      <Modal visible={visible} animationType="slide">
-        <View className="flex-1 justify-center items-center">
-          {options.map((option) => (
-            <TouchableOpacity
-              key={option.value}
-              onPress={() => handleSelect(option.value)}
-              className="p-2"
-            >
-              <Text>{option.label}</Text>
-            </TouchableOpacity>
-          ))}
-          //{" "}
-          <TouchableOpacity
-            onPress={() => setVisible(false)}
-            className="mt-4 p-2 bg-gray-300 rounded"
-          >
-            <Text>Close</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-    </View>
-  );
-}
-const handleSelectGender = (gender) => {
-  setGenderValue(gender);
-};
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function AddPatients() {
-  const [dateOfBirth, setDateOfBirth] = useState("");
-
   const initialPatientInfo = {
     firstName: "",
     surName: "",
@@ -70,30 +30,36 @@ export default function AddPatients() {
     phoneNumber: "",
     recordNumber: "",
     notes: "",
-
-    dateOfBirth: new Date(),
+    selectedSex: "Select Sex",
   };
-  const [selectedValue, setSelectedValue] = useState(null);
-  const options = ["Male", "Female", "Other"];
+  const [touchedFields, setTouchedFields] = useState({});
 
+  const [isPressed, setIsPressed] = useState(false);
   const [selectedSex, setSelectedSex] = useState(null);
 
   const [patientInfo, setPatientInfo] = useState(initialPatientInfo);
 
+  const [errors, setErrors] = useState({});
+
   const [showTextInput, setShowTextInput] = useState(false);
-
-  const [genderValue, setGenderValue] = useState(null);
-
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
 
   const handleChange = (field, value) => {
     setPatientInfo({
       ...patientInfo,
       [field]: value,
     });
+
+    // setErrors({
+    //   ...errors,
+    //   [field]: "",
+    // });
   };
 
   const handleSelectSex = (sex) => {
+    // setErrors({ ...errors, selectedSex: setSelectedSex("") });
     setSelectedSex(sex);
     setPatientInfo({
       ...patientInfo,
@@ -115,211 +81,203 @@ export default function AddPatients() {
     // setErrors({});
     setShowTextInput(false);
     setDateOfBirth(null);
-    // setGenderValue("Select Gender");
-    setSelectedValue("Select Gender");
   };
-
   const handleRecordNumber = () => {
     return console.log("handleRecordNumber Called...");
   };
 
   const handleValidateAndSubmit = () => {
-    console.log("validate and then handle submit");
+    console.log("i will validate and thhe handle submit");
   };
-  const Dropdown = ({ options, onSelect, selectedValue }) => {
-    const [visible, setVisible] = useState(false);
-    const [tempSelectedValue, setTempSelectedValue] = useState(selectedValue);
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  };
 
-    const handleSelect = (option) => {
-      onSelect(option);
-      setVisible(false);
-    };
-    const openDropdown = () => {
-      setTempSelectedValue(selectedValue);
-      setVisible(true);
-    };
+  const onChangeDate = ({ type }, selectedDate) => {
+    if (type === "set") {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+      setDateOfBirth(formatDate(currentDate));
+      if (Platform.OS === "android") {
+        toggleDatePicker();
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
 
-    const closeDropdown = () => {
-      setTempSelectedValue(null);
-      setVisible(false);
-    };
-
-    return (
-      <View className="mb-3 mt-5 ">
-        <TouchableOpacity
-          className="flex-row items-center justify-between p-2 border-2  border-gray-300 rounded-md  w-full relative"
-          onPress={() => setVisible(!visible)}
-        >
-          <View className="flex flex-row gap-[155]">
-            <Text className="text-black">
-              {selectedValue ? selectedValue : "Select Gender"}
-            </Text>
-            <Text className=" w-4/6 text-black">{visible ? "▲" : "▼"}</Text>
-          </View>
-        </TouchableOpacity>
-
-        {visible && (
-          <Modal
-            transparent={true}
-            visible={visible}
-            animationType="none"
-            onRequestClose={closeDropdown}
-            // {() => setVisible(false)}
-          >
-            <TouchableOpacity
-              className="flex-1"
-              onPress={closeDropdown}
-              // {() => setVisible(false)}
-              activeOpacity={1}
-            >
-              <View className="absolute bottom-[275]  right-7 bg-white border border-gray-300 rounded-sm w-[245] px-2 ">
-                <FlatList
-                  data={options}
-                  keyExtractor={(item) => item}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      className="p-2 border-b border-gray-300 "
-                      onPress={() => handleSelect(item)}
-                    >
-                      <Text>{item}</Text>
-                    </TouchableOpacity>
-                  )}
-                />
-              </View>
-            </TouchableOpacity>
-          </Modal>
-        )}
-      </View>
-    );
+  const formatDate = (date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day < 10 ? "0" + day : day}-${
+      month < 10 ? "0" + month : month
+    }-${year}`;
   };
 
   return (
     <SafeAreaView className="h-full">
-      {/* <ScrollView> */}
-      <View className="    min-h-[90vh] w-full  py-3">
-        <View className="  flex-row justify-between px-2 py-2  w-[90vw]  mx-auto  ">
-          <Text className="text-[15px] font-medium">Add New Patient</Text>
-          <View className=" buttonContainer  flex-row gap-8 mx-auto ">
-            <TouchableOpacity
-              className=" cancelBtn px-2 py-2 rounded border border-[#0000AC] bg-gray-100"
-              onPress={handleCancel}
-            >
-              <Text className="  text-[#0000AC] font-semibold">Cancel</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className="saveBtn px-3 py-2 rounded bg-[#0000AC]"
-              onPress={handleValidateAndSubmit}
-            >
-              <Text className="text-white font-semibold ">Save</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View className=" formContainer w-[95vw] bg-[#F9F9F9] p-4 rounded-md  mt-5 mx-auto ">
-          <View className=" recordNumberContainer  mt-5 flex-row gap-4  ">
-            <Text className="text-base text-[#4F4F4F]">Record Number</Text>
-            <View className="recordNumberContainer mb-5">
-              <Text className="mx-auto">Record number will be assigned </Text>
-              <Text>automatically when you save.</Text>
-              <TouchableOpacity className=" py-2 border-2 border-[#E0E0E0] w-2/3 rounded-md items-center mt-3 mb-3">
-                <Text className="text-black font-semibold  ">
-                  Assign manually
-                </Text>
+      <ScrollView>
+        <View className="    min-h-[90vh] w-full  py-3">
+          <View className="  flex-row justify-between px-2 py-2  w-[90vw]  mx-auto  ">
+            <Text className="text-[15px] font-medium">Add New Patient</Text>
+            <View className=" buttonContainer  flex-row gap-8 mx-auto ">
+              <TouchableOpacity
+                className=" cancelBtn px-2 py-2 rounded border border-[#0000AC] bg-gray-100"
+                onPress={handleCancel}
+              >
+                <Text className="  text-[#0000AC] font-semibold">Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="saveBtn px-3 py-2 rounded bg-[#0000AC]"
+                onPress={handleValidateAndSubmit}
+              >
+                <Text className="text-white font-semibold ">Save</Text>
               </TouchableOpacity>
             </View>
           </View>
-
-          <View className=" firstNameContainer flex-row items-center  mb-5 ">
-            <Text className="w-2/6 text-base text-[#4F4F4F] ">Firstname</Text>
-            <View className="iconWrap   relative  flex-row   w-full">
-              <TextInput
-                className="w-4/6 p-1 px-3  bg-white  border-2 border-[#E0E0E0] rounded-md"
-                value={patientInfo.firstName}
-                onChangeText={(value) => handleChange("firstName", value)}
-              />
-              <View className="absolute  left-[210]  top-3 transform -translate-y-1/2">
-                <Pressable
-                  className="font-extrabold"
-                  onPress={handleCancelFirstName}
-                >
-                  <AntDesign name="close" size={20} color="black" />
-                </Pressable>
+          <View className=" formContainer w-[95vw] bg-[#F9F9F9] p-4 rounded-md  mt-5 mx-auto ">
+            <View className=" recordNumberContainer  mt-5 flex-row gap-4  ">
+              <Text className="text-base text-[#4F4F4F]">Record Number</Text>
+              <View className="recordNumberContainer mb-5">
+                <Text className="mx-auto">Record number will be assigned </Text>
+                <Text>automatically when you save.</Text>
+                <TouchableOpacity className=" py-2 border-2 border-[#E0E0E0] w-2/3 rounded-md items-center mt-3 mb-3">
+                  <Text className="text-black font-semibold  ">
+                    Assign manually
+                  </Text>
+                </TouchableOpacity>
               </View>
             </View>
-          </View>
 
-          <View className=" surnameContainer flex-row items-center  mb-5 ">
-            <Text className="w-2/6 text-base text-[#4F4F4F] ">Surname</Text>
-            <View className="iconWrap   relative  flex-row   w-full">
-              <TextInput
-                className="w-4/6 p-1 px-3 bg-white  border-2 border-[#E0E0E0] rounded-md"
-                value={patientInfo.surName}
-                onChangeText={(value) => handleChange("surName", value)}
-              />
-              <View className="absolute font-extrabold left-[210]  top-3 transform -translate-y-1/2">
-                <Pressable className="" onPress={handleCancelSurname}>
-                  <AntDesign name="close" size={20} color="black" />
-                </Pressable>
+            <View className=" firstNameContainer flex-row items-center  mb-5 ">
+              <Text className="w-2/6 text-base text-[#4F4F4F] ">Firstname</Text>
+              <View className="iconWrap   relative  flex-row   w-full">
+                <TextInput
+                  className="w-4/6 p-1 px-3  bg-white  border-2 border-[#E0E0E0] rounded-md"
+                  value={patientInfo.firstName}
+                  onChangeText={(value) => handleChange("firstName", value)}
+                />
+                <View className="absolute  left-[210]  top-3 transform -translate-y-1/2">
+                  <Pressable
+                    className="font-extrabold"
+                    onPress={handleCancelFirstName}
+                  >
+                    <AntDesign name="close" size={20} color="black" />
+                    {/* <EvilClose name="close" size={25} className="pr-30" /> */}
+                  </Pressable>
+                </View>
               </View>
             </View>
-          </View>
 
-          <View className="dateContainer flex-row  items-center">
-            <Text className="w-1/6 text-base text-[#4F4F4F]">D.O.B</Text>
+            <View className=" surnameContainer flex-row items-center  mb-5 ">
+              <Text className="w-2/6 text-base text-[#4F4F4F] ">Surname</Text>
+              <View className="iconWrap   relative  flex-row   w-full">
+                <TextInput
+                  className="w-4/6 p-1 px-3 bg-white  border-2 border-[#E0E0E0] rounded-md"
+                  value={patientInfo.surName}
+                  onChangeText={(value) => handleChange("surName", value)}
+                />
+                <View className="absolute font-extrabold left-[210]  top-3 transform -translate-y-1/2">
+                  <Pressable className="" onPress={handleCancelSurname}>
+                    <AntDesign name="close" size={20} color="black" />
+                  </Pressable>
+                </View>
+              </View>
+            </View>
 
-            <FontAwesome5 name="calendar-alt" size={25} color="black" />
+            <View className="datePickerContainer flex-row  items-center">
+              <Text className="w-1/6 text-base text-[#4F4F4F]">D.O.B</Text>
 
-            <TextInput
-              className="w-4/6 p-1 px-3  ml-10  bg-white  border-2 border-[#E0E0E0] rounded-md"
-              value={patientInfo.dateOfBirth}
-              onChangeText={(value) => handleChange("dateOfBirth", value)}
-              placeholder="dd-mm-yyyy"
-              keyboardType="numeric"
-            />
-          </View>
-          <View>
+              <FontAwesome5
+                name="calendar-alt"
+                size={25}
+                color="black"
+                // className="mr-2"
+              />
+              {showPicker && (
+                <DateTimePicker
+                  mode="date"
+                  display="spinner"
+                  value={date}
+                  onChange={onChangeDate}
+                  className="mr-2"
+                />
+              )}
+
+              {showPicker && Platform.OS === "ios" && (
+                <View className="flex flex-row justify-around">
+                  <TouchableOpacity onPress={toggleDatePicker}>
+                    <Text className="bg-blue-500 w-2/6 rounded-lg border-2 border-red-300 text-center p-2">
+                      Cancel
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={toggleDatePicker}>
+                    <Text className="bg-blue-500 w-2/6 rounded-lg border-2 border-green-300 text-center p-2">
+                      Ok
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+              {!showPicker && (
+                <Pressable onPress={toggleDatePicker}>
+                  <TextInput
+                    className="w-[240] p-1 px-3 bg-white  border-2 border-[#E0E0E0] rounded-md text-black font-bold ml-10"
+                    placeholderTextColor="black"
+                    editable={false}
+                    value={dateOfBirth}
+                  />
+                </Pressable>
+              )}
+            </View>
+
             <View className="genderContainer flex-row mt-2 mb-5 items-center">
               <Text className="w-2/6 text-base text-[#4F4F4F]">Gender</Text>
-              <View className="w-4/6">
-                <Dropdown
-                  options={options}
-                  selectedValue={selectedValue}
-                  onSelect={setSelectedValue}
-                />
-              </View>
+              <Picker
+                style={{ flex: 1, height: 30 }}
+                // className=" flex-1 h-30 hover:bg-gray-400 rounded-sm"
+                selectedValue={selectedSex}
+                onValueChange={(itemValue) => handleSelectSex(itemValue)}
+              >
+                <Picker.Item label="Select Gender" value={null} />
+                <Picker.Item label="Male" value="male" />
+                <Picker.Item label="Female" value="female" />
+                <Picker.Item label="Other" value="other" />
+              </Picker>
+            </View>
+
+            <View className=" diagnosisContainer flex-row items-center  mb-5">
+              <Text className="w-2/6  text-base text-[#4F4F4F]">Diagnosis</Text>
+              <TextInput
+                className="w-4/6 p-1 px-3 bg-white border-2 border-[#E0E0E0] rounded-md"
+                value={patientInfo.diagnosis}
+                onChangeText={(value) => handleChange("diagnosis", value)}
+              />
+            </View>
+
+            <View className=" phoneNumberContainer flex-row items-center  mb-5">
+              <Text className="w-2/6 text-[#4F4F4F]">Phone Number</Text>
+
+              <TextInput
+                className="w-4/6 p-1 px-3 bg-white border-2 border-[#E0E0E0] rounded-md "
+                value={patientInfo.phoneNumber}
+                onChangeText={(value) => handleChange("phoneNumber", value)}
+                keyboardType="number-pad"
+              />
+            </View>
+            <View className="notesContainer flex-row  items-center mb-5">
+              <Text className="w-2/6 text-base  text-[#4F4F4F]">Notes</Text>
+              <TextInput
+                className="w-4/6 p-3 px-3 bg-white h-20 border-2 border-[#E0E0E0] rounded-md   "
+                multiline={true}
+                textAlignVertical="top"
+                value={patientInfo.notes}
+                onChangeText={(value) => handleChange("notes", value)}
+              />
             </View>
           </View>
-
-          <View className=" diagnosisContainer flex-row items-center  mb-5">
-            <Text className="w-2/6  text-base text-[#4F4F4F]">Diagnosis</Text>
-            <TextInput
-              className="w-4/6 p-1 px-3 bg-white border-2 border-[#E0E0E0] rounded-md"
-              value={patientInfo.diagnosis}
-              onChangeText={(value) => handleChange("diagnosis", value)}
-            />
-          </View>
-
-          <View className=" phoneNumberContainer flex-row items-center  mb-5">
-            <Text className="w-2/6 text-[#4F4F4F]">Phone Number</Text>
-
-            <TextInput
-              className="w-4/6 p-1 px-3 bg-white border-2 border-[#E0E0E0] rounded-md "
-              value={patientInfo.phoneNumber}
-              onChangeText={(value) => handleChange("phoneNumber", value)}
-              keyboardType="number-pad"
-            />
-          </View>
-          <View className="notesContainer flex-row  items-center mb-5">
-            <Text className="w-2/6 text-base  text-[#4F4F4F]">Notes</Text>
-            <TextInput
-              className="w-4/6 p-3 px-3 bg-white h-20 border-2 border-[#E0E0E0] rounded-md   "
-              multiline={true}
-              textAlignVertical="top"
-              value={patientInfo.notes}
-              onChangeText={(value) => handleChange("notes", value)}
-            />
-          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
